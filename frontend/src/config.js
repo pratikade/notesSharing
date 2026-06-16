285740927b1c98ccd48f0b8c26724bff8abfb2e1
Pratik Ashok Ade <137700845+pratikade@users.noreply.github.com>
2026-06-16 11:59:53 +0530

    Add configuration file for API URL

---
+++ frontend/src/config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    // Use your Render backend URL here
+    API_URL: 'https://notessharing-l5mq.onrender.com' // Replace with your actual Render URL
+};
+
+export default config;
